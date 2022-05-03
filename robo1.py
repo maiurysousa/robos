d42e80649819577c1e8d442fc21ac3c72384b4c3
maiurysousa <git>
2022-05-03 15:17:04 -0300

    iniciando robo1

---
+++ robo1.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+print("Iniciando robô...\n")
+
+driver = webdriver.Chrome('C:\Users\maiury.nascimento\Desktop\Robos\chromedriver')
+driver.get("https://registro.br/")
+time.sleep(2) #Dormir 2 segundos
+driver.close()
